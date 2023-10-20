3ecd99d8974f042e9fa3dab51f25c920dffd5d66
menna544 <116039213+menna544@users.noreply.github.com>
2023-10-20 23:12:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 var images = document.querySelector(".img")
-var imgpho=["images/images.jpg","images/images1.jpg","images/photo_2023-10-13_01-10-38.jpg"]
-var i=0;
-function displayNextImage() {
-    i++;
-    if( i>= imgpho.length){
-    i=0;
-    images.src=imgpho[i];
-}else{
-images.src=imgpho[i]
+var imgpho=["images/photo_2023-10-13_01-10-38.jpg","images/images.jpg","images/images1.jpg","images/images (2).jpg"]
+var num=0;
+function next(){
+    if( num >=imgpho.length-1){
+        num=0;
+    }
+   else{
+    num++;
+   } 
+   console.log(num);
+   again();
 }
+function back(){
+    if(num<=0)
+        num=imgpho.length-1;
+     else{
+        num--;
+     }
+     console.log(num);
+        again();
 }
-function displayPreviousImage() {
-    i--;
-    if(i<0){
-        i=imgpho.length-1;
-        images.src=imgpho[i];
-}else{
-    images.src=imgpho[i];
-}
+function again(){
+    return images.setAttribute('src',imgpho[num]);
 }
